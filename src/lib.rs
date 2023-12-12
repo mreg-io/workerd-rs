35b93577b4ef4dc4c9b791b01689eb60b5445d2a
趙子賢 <danny900714@gmail.com>
2023-12-12 23:11:34 +0800

    Add version function to Workerd

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
+use std::process::Command;
+
+use anyhow::{anyhow, Result};
+
 const WORKERD_PATH: &str = env!("WORKERD_PATH");
+
+pub struct Workerd {}
+
+impl Workerd {
+    pub fn version() -> Result<String> {
+        let output = Command::new(WORKERD_PATH).arg("--version").output()?;
+        let output_str = String::from_utf8_lossy(&output.stdout);
+        let raw_version = output_str
+            .split_whitespace()
+            .last()
+            .ok_or(anyhow!("Cannot parse workerd version output"))?;
+        Ok(format!("1.{}.0", raw_version.replace("-", "")))
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
+
     use super::*;
 
     #[test]
@@ -23,5 +42,11 @@ mod tests {
         path.push("bin");
         path.push("workerd");
         assert_eq!(WORKERD_PATH, path.to_str().unwrap());
+    }
+
+    #[test]
+    fn test_workerd_version() {
+        let version = Workerd::version().unwrap();
+        assert_eq!(version, env!("CARGO_PKG_VERSION"));
     }
 }
